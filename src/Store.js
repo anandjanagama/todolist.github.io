efbaf4d35fd0f09aacf06ea1bc77cafd80d70b73
anandjanagama <84390800+anandjanagama@users.noreply.github.com>
2021-05-29 15:26:06 +0530

    Revert "to do list app"
    This reverts commit 460884ad87f3afc22e8d8ab050214919b8e25339.

--- src/Store.js
+++
@@ -1,6 +0,0 @@
-import mainReducer from './reducers/mainReducer';
-import {createStore} from 'redux'
-
-const Store= createStore(mainReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
-export default Store;
--- src/reducers/mainReducer.js
+++
@@ -1,6 +0,0 @@
-import {combineReducers} from 'redux';
-import reducers from './reducers';
-
-const mainReducer=combineReducers({reducers })
-
-export default mainReducer;
